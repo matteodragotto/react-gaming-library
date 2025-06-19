aba019258a9857064d993e2a90a103e45c115a58
matteodragotto <matteodrag8@gmail.com>
2025-06-19 18:04:52 +0200

    [feat] filter buttons on/off on click - genres and platforms can now be selected together

--- src/components/SearchFilters.jsx
+++ src/components/SearchFilters.jsx
@@ -21,13 +21,40 @@ const SearchFilters = ({ genres, platforms }) => {
   }
 
   const handleGenreClick = (genreId) => {
-    setSelectedGenre(genreId);
-    searchGames(`genre_id=${genreId}`);
+    if (selectedGenre === genreId) {
+      setSelectedGenre(null);
+      if (!selectedPlatform) {
+        fetchGames(1);
+      } else {
+        searchGames(`platform_id=${selectedPlatform}`);
+      }
+    } else {
+      setSelectedGenre(genreId);
+      if (selectedPlatform) {
+        searchGames(`genre_id=${genreId}&platform_id=${selectedPlatform}`);
+      } else {
+        searchGames(`genre_id=${genreId}`);
+      }
+    }
   };
 
+
   const handlePlatformClick = (platformId) => {
-    setSelectedPlatform(platformId);
-    searchGames(`platform_id=${platformId}`);
+    if (selectedPlatform === platformId) {
+      setSelectedPlatform(null);
+      if (!selectedGenre) {
+        fetchGames(1);
+      } else {
+        searchGames(`genre_id=${selectedGenre}`);
+      }
+    } else {
+      setSelectedPlatform(platformId);
+      if (selectedGenre) {
+        searchGames(`genre_id=${selectedGenre}&platform_id=${platformId}`);
+      } else {
+        searchGames(`platform_id=${platformId}`);
+      }
+    }
   };
 
   const handleResetClick = () => {
@@ -35,7 +62,7 @@ const SearchFilters = ({ genres, platforms }) => {
     setSelectedPlatform(null);
     setAreGenresVisible(false);
     setArePlatformsVisible(false);
-    fetchGames(1); // Reset to the first page of games
+    fetchGames(1);
   };
 
   return (
@@ -59,30 +86,35 @@ const SearchFilters = ({ genres, platforms }) => {
           className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-500 transition-colors"
           onClick={() => handleResetClick()}
         >
-          Mostra Tutti
+          Rimuovi filtri
         </button>
       </div>
 
 
-      <div className="flex flex-wrap justify-center gap-4 my-5">
-        {areGenresVisible && genres.map((genre) => (
-          <button
-            key={genre.id}
-            className={`text-white px-4 py-2 rounded hover:bg-gray-700 transition-colors ${selectedGenre === genre.id ? 'bg-blue-600' : 'bg-gray-800'}`}
-            onClick={() => handleGenreClick(genre.id)}
-          >
-            {genre.name}
-          </button>
-        ))}
-        {arePlatformsVisible && platforms.map((platform) => (
-          <button
-            key={platform.id}
-            className={`text-white px-4 py-2 rounded hover:bg-gray-700 transition-colors ${selectedPlatform === platform.id ? 'bg-blue-600' : 'bg-gray-800'}`}
-            onClick={() => handlePlatformClick(platform.id)}
-          >
-            {platform.name}
-          </button>
-        ))}
+      <div className="flex flex-col items-center gap-2 my-5">
+        <div className="border rounded-xs flex flex-wrap justify-center items-center gap-2 p-4">
+          {areGenresVisible && genres.map((genre) => (
+            <button
+              key={genre.id}
+              className={`text-white px-4 py-2 rounded hover:bg-gray-700 transition-colors ${selectedGenre === genre.id ? 'bg-blue-600' : 'bg-gray-800'}`}
+              onClick={() => handleGenreClick(genre.id)}
+            >
+              {genre.name}
+            </button>
+          ))}
+        </div>
+        <div className="border rounded-xs flex flex-wrap justify-center items-center gap-2 p-4">
+          {arePlatformsVisible && platforms.map((platform) => (
+            <button
+              key={platform.id}
+              className={`text-white px-4 py-2 rounded hover:bg-gray-700 transition-colors ${selectedPlatform === platform.id ? 'bg-blue-600' : 'bg-gray-800'}`}
+              onClick={() => handlePlatformClick(platform.id)}
+            >
+              {platform.name}
+            </button>
+          ))}
+        </div>
+
       </div>
 
     </div>
